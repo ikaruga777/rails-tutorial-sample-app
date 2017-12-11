215c332ebd6dfac3b5b9ed6bd59a059e93d5f567
ikaruga <ikaruga777@gmail.com>
2017-12-12 08:27:41 +0900

    4.1 カスタムヘルパーを作る

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #タイトルを入れるよ
+  def full_title(page_title="")
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty? then
+      return base_title
+    else
+      return page_title + " | " + base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,7 +8,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
@@ -22,5 +22,4 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title" , "About | #{@base_title}"
   end
-
 end
