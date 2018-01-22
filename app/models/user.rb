ad4ef656945fa3023df72789c4c1d974dc2670c3
ikaruga <ikaruga777@gmail.com>
2018-01-22 17:08:55 +0900

    パスワードのvalidationを追加する。
    - 6文字以上であること
    - 入力は必須である

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # DBの種類によってindexが大文字小文字区別するしないを考えなくて良いようにする
-  before_save { self.email.downcase! }
+  before_save { email.downcase! }
   #validatesはメソッド、特定のハッシュに対してプロパティを設定する。からコロンの位置が絶妙な感じになってる
   validates :name, presence: true, length: { maximum: 50 }
   
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
