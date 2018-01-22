421d1c54f8a3850c038da5d14d81b3f1eeaad3b8
ikaruga <ikaruga777@gmail.com>
2018-01-22 15:42:38 +0900

    6.2.3 文字列長の制限とテストの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   #validatesはメソッド、特定のハッシュに対してプロパティを設定する。からコロンの位置が絶妙な感じになってる
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 50}
+  validates :email, presence: true, length: { maximum: 255 }
 end
