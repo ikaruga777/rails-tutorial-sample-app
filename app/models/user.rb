694bee8a7d6ea295daba449da286c1e47e6c8fdb
ikaruga <ikaruga777@gmail.com>
2018-01-22 16:11:59 +0900

    6.2.4 emailのValidattionを追加する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   #validatesはメソッド、特定のハッシュに対してプロパティを設定する。からコロンの位置が絶妙な感じになってる
-  validates :name, presence: true, length: { maximum: 50}
-  validates :email, presence: true, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 50 }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup 
-    @user = User.new(name: "Example User", email: "user.example.com")
+    @user = User.new(name: "Example User", email: "user@example.com")
   end
 
   test "shuld be valid " do
@@ -27,5 +27,22 @@ class UserTest < ActiveSupport::TestCase
   test "email should not be too long" do
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
+  end
+  
+  test "email validation shuould accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      #assert の第二引数にエラーメッセージ入れて異常ケースを特定する
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation shoul reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example foo@bar_baz.com foo@bar+baz.com fooo@bar..com]
+    invalid_addresses.each do | invalid_address |
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
   end
 end
