ced3746a99d693b2bacb52cb8cbe4125d91bf5e1
ikaruga <ikaruga777@gmail.com>
2018-01-29 08:17:13 +0900

    User登録後にUserを表示する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # success
+      redirect_to user_url(@user)
     else
       render 'new'
     end
