be14b78fe4fc91716a49a1982e6b123a172d1b0d
ikaruga <ikaruga777@gmail.com>
2018-02-06 08:22:33 +0900

    ログインページ表面実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create 
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン後リダイレクト
+    else
+      # エラー
+      render 'new'
+    end
+  end
+
+  def destory
+  end
+
 end
