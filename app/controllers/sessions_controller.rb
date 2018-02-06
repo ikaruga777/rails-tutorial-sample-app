e1c3e92812898a789ea0d33a7d526e18e6123b2b
ikaruga <ikaruga777@gmail.com>
2018-02-07 08:06:38 +0900

    ログイン失敗メッセージを残さない

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       # ログイン後リダイレクト
     else
       # エラー
-      flash[:danger] = "Invalid email/password combination"
+      flash.now[:danger] = "Invalid email/password combination"
       render 'new'
     end
   end
