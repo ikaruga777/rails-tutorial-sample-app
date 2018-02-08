d523e8a698218c586dc9b9b918fb80266b5506f6
ikaruga <ikaruga777@gmail.com>
2018-02-09 08:28:50 +0900

    正しくログインできることを確認するテストを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     # ログインページアクセス
     get login_path
@@ -14,5 +19,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     #　ページ遷移したらflashが消えてる?
     get root_path
     assert flash.empty?
+  end
+
+  test 'login with valid information' do
+    get login_path
+    assert_template 'sessions/new'
+
+    post login_path params: { session: { email: @user.email, password: "password"} }
+    follow_redirect!
+    assert_template 'users/show'
+    # ヘッダーの切り替えができてる
+    assert_select "a[href=?]", login_path, count:0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
