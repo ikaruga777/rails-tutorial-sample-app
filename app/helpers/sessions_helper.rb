2038a7144ed049699a46c0b74f6c545eb2c8789c
ikaruga <ikaruga777@gmail.com>
2018-04-09 08:22:44 +0900

    セッションに遷移元のページを記憶するメソッドの実装
    ログイン後に再度リダイレクトさせるため

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -44,4 +44,17 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # セッションに記憶していたURLにリダイレクトする
+  # セッションに無かった場合は引数で渡されたパスにリダイレクトする
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLをセッションに記憶させとく
+  # redirect_back_orで取り出せる
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
 end
