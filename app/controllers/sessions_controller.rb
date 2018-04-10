944b5352a3231b0139bdb86c7bbc535235ffdc0f
ikaruga <ikaruga777@gmail.com>
2018-04-11 00:14:57 +0900

    ログイン後にログイン前に表示しようとしてたページにリダイレクトさせる

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       # ログイン後リダイレクト
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      redirect_back_or @user
     else
       # エラー
       flash.now[:danger] = "Invalid email/password combination"
