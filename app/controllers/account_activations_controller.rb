83a71d8049df1d0e3fcc52d241157a50d1ef8a5d
ikaruga <ikaruga777@gmail.com>
2018-05-03 16:36:20 +0900

    ユーザの有効化の実装
    認証URLからユーザの有効化を行う
    有効でないユーザのログインを弾く

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,18 @@
 class AccountActivationsController < ApplicationController
+  
+  # 認証URLからのユーザ有効化
+  def edit
+    user = User.find_by(email: params[:email])
+    # 有効化は一度のみ可能にする
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated,     true)
+      user.update_attribute(:activated_at,  Time.zone.now)
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
+  end
 end
