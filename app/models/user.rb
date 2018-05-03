68aefe5c0eeeee3c980225d03684920ea15c55ce
ikaruga <ikaruga777@gmail.com>
2018-05-03 17:17:12 +0900

    DB編集を1回ですませる

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,8 +43,8 @@ class User < ApplicationRecord
 
   # アカウントの有効化
   def activate
-    update_attribute(:activated,     true)
-    update_attribute(:activated_at,  Time.zone.now)
+    update_columns( activated:    true,
+                    activated_at:  Time.zone.now)
   end
 
   # アカウント有効化用の認証メールを送信する
