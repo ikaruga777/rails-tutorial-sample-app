f64444b52b25d9eb118f5c2de063246d68d76ec7
ikaruga <ikaruga777@gmail.com>
2018-05-11 08:18:16 +0900

    パスワードリセットフォームの実装

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,5 @@ Rails.application.routes.draw do
   
   resources :users
   resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new,:craete,:edit,:update]
+  resources :password_resets,     only: [:new,:create,:edit,:update]
 end
