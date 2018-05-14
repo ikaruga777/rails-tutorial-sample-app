9dc91747f8474940a1ed95f84926bf8a68d1ba99
ikaruga <ikaruga777@gmail.com>
2018-05-15 08:15:24 +0900

    パスワードリマインダーのSubmit処理実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   # DBの種類によってindexが大文字小文字区別するしないを考えなくて良いようにする
   before_save :downcase_email
   before_create :create_activation_digest
@@ -51,6 +51,11 @@ class User < ApplicationRecord
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
   private
 
   def downcase_email
@@ -61,5 +66,12 @@ class User < ApplicationRecord
   def create_activation_digest
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
+  end
+
+  # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
   end
 end
