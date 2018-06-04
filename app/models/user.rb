752de81299be46fcec88e9c8548d9fdb5f1dd782
ikaruga <ikaruga777@gmail.com>
2018-06-04 21:30:36 +0900

    userとmicropostが関連付けできるようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
+  has_many :microposts
   # DBの種類によってindexが大文字小文字区別するしないを考えなくて良いようにする
   before_save :downcase_email
   before_create :create_activation_digest
