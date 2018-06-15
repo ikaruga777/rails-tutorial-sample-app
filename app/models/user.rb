60a7340a29017eda0702c670437b729828136302
ikaruga <ikaruga777@gmail.com>
2018-06-16 01:25:38 +0900

    フォローしているユーザを取得できる

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: followed
   # 指定した文字列の暗号化
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
