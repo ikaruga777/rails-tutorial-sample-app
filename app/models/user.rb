c659322c3bbe583eb2af4b588eb2c345b4b73d78
ikaruga <ikaruga777@gmail.com>
2018-06-19 08:07:23 +0900

    ユーザのフォロワーを吐き出すメソッドの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   # 指定した文字列の暗号化
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
