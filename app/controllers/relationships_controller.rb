7fc25a42016ce692959ac898fb2cebf8fd559d7d
ikaruga <ikaruga777@gmail.com>
2018-06-20 23:47:05 +0900

    フォローボタン、アンフォローボタンを実装する

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,15 @@
 class RelationshipsController < ApplicationController
+  
+  def create
+    user = User.find(params[:followed_id])
+    # ログインしているユーザがフォローする
+    current_user.follow(user)
+    redirect_to user
+  end
+
+  def destroy
+    user = Relationship.find(params[:id]).followed
+    current_user.unfollow(user)
+    redirect_to user
+  end
 end
