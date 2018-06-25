cd47819eefb99ae2c1668fd7b97ceb0419dbe610
ikaruga <ikaruga777@gmail.com>
2018-06-26 08:16:49 +0900

    ユーザの情報を返すQueryを作る
    https://qiita.com/kielze/items/b2168d7214992e5d37bf　をみた。

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,11 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
-  # Add root-level fields here.
-  # They will be entry points for queries on your schema.
 
-  # TODO: remove me
+  field :user, !Types::UserType do
+    resolve ->(obj, args, ctx) {
+      ctx[:current_user]
+    }
+  end
   field :testField, types.String do
     description "An example field added by the generator"
     resolve ->(obj, args, ctx) {
