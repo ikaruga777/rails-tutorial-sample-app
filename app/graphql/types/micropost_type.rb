985956a1125fed69de7bf097439825744578cb8a
ikaruga <ikaruga777@gmail.com>
2018-06-29 17:01:02 +0900

    DateTimeを返せるようにする

--- app/graphql/types/micropost_type.rb
+++ app/graphql/types/micropost_type.rb
@@ -4,5 +4,5 @@ Types::MicropostType = GraphQL::ObjectType.define do
   field :content, !types.String
   field :user_id, !types.ID
   field :picture, !types.String
-  #field :created_at, !types.Date
+  field :created_at, Types::DateTimeType
 end
