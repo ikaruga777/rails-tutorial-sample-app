5f6641c05025e163c8c7a0559f158132d6655fbd
ikaruga <ikaruga777@gmail.com>
2018-07-04 16:21:53 +0900

    リレーションをGraphQLで返せるようにする
    has_manyな関係であっても生やせる。
    リレーション挟んだ先でも普通にschemaに落としてくれる。
    持ってくる先の型のconnection_typeを指定することに注意する。

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,7 +1,12 @@
-Types::UserType = GraphQL::ObjectType.define do
-  name "User"
-  field :id, !types.ID
-  field :name, !types.String
-  field :email, !types.String
-  connection :microposts, !Types::MicropostType.connection_type
+module Types
+    UserType = GraphQL::ObjectType.define do
+      name "User"
+      description "ユーザ情報だよ"
+      field :id, !types.ID
+      field :name, !types.String
+      field :email, !types.String
+      connection :microposts, MicropostType.connection_type
+      connection :following, UserType.connection_type
+      connection :followers, UserType.connection_type
+  end
 end
