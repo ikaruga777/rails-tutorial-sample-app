d15933ed4bd6e49cf0fa3198c7ea24b42789cd2d
ikaruga <ikaruga777@gmail.com>
2018-09-17 15:54:36 +0900

    add action

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -12,4 +12,13 @@ ActiveAdmin.register User do
 #   permitted
 # end
 
+  action_item :sugee_action, only: :show do
+    link_to "sugeeee", {action: :sugee}, method: :put
+  end
+
+  member_action :sugee, method: :put do
+    resource.name = "sugee"
+    resource.save!
+    redirect_to resource_path, notice: "sugee!"
+  end
 end
