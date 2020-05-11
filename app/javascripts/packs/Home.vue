00787458d4ac54a53963e7d17818a1d29ab955f1
ikaruga <ikaruga777@gmail.com>
2020-05-12 00:05:40 +0900

    れんしゅうもんだい1まで

--- app/javascripts/packs/Home.vue
+++ app/javascripts/packs/Home.vue
@@ -1,7 +1,34 @@
 <template>
-  <p>Hello, world!</p>
+  <div>
+    <div v-if="isLoading">
+      ろーてぃんぐちゅう
+    </div>
+    <div v-else>
+      <div v-if="feeds.length > 0 ">
+        <p v-for="feed in feeds" v-bind:key="feed.id">
+          {{ feed.content }}
+        </p>
+      </div>
+      <div v-else>
+        ふぃーどがないよ
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      isLoading: true,
+      feeds: []
+    };
+  },
+  async mounted() {
+    const res = await axios.get('/feeds.json');
+    this.feeds = res.data;
+    this.isLoading = false;
+  }
+}
 </script>
