5fe2a819b7371c55f15c6944dd452dd7c48a6ac1
ikaruga <ikaruga777@gmail.com>
2020-06-08 09:09:29 +0900

    feed.jsonをgetして出力する

--- app/javascripts/packs/Home.vue
+++ app/javascripts/packs/Home.vue
@@ -1,7 +1,29 @@
 <template>
-  <p>Hello, world!</p>
+  <div>
+    <div v-if="feeds.length > 0">
+      <p v-for "feed in feeds" v-bind:key="feed.id">
+        あるぜ
+        {{ feed.content }}
+      </p>
+    </div>
+    <div v-else>
+      ないぜ
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      feeds: []
+    };
+  },
+  async mounted() {
+    const response = await axios.get('/feeds.json');
+    this.feeds = response.data;
+  }
+};
 </script>
