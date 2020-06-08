66ef04035e9ccdd42d840af87f73ffadf1d1a973
ikaruga <ikaruga777@gmail.com>
2020-06-08 09:18:54 +0900

    練習問題1まで

--- app/javascripts/packs/Home.vue
+++ app/javascripts/packs/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <div v-if="feeds.length > 0">
-      <p v-for="feed in feeds" v-bind:key="feed.id">
-        {{ feed.content }}
-      </p>
+    <div v-if="isLoading">
     </div>
     <div v-else>
-      ないぜ
+      <div v-if="feeds.length > 0">
+          <p v-for="feed in feeds" v-bind:key="feed.id">
+            {{ feed.content }}
+          </p>
+        </div>
+        <div v-else>
+          ないぜ
+        </div>
     </div>
   </div>
 </template>
@@ -17,12 +21,14 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      feeds: []
+      feeds: [],
+      isLoading: true
     };
   },
   async mounted() {
     const response = await axios.get('/feeds.json');
     this.feeds = response.data;
+    this.isLoading = false
   }
 };
 </script>
